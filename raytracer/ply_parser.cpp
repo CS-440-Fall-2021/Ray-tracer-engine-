0eb7847bbfd5bc657c5515b7f309fa4145b4c3a8
hasan <hasannaseem2@gmail.com>
2021-12-19 19:27:14 +0500

    ply parser

---
+++ raytracer/ply_parser.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include "happly-master/happly.h"
+#include "utilities/Point3D.hpp"
+using namespace std;
+
+int main(){
+
+    happly::PLYData plyIn("dragon_vrip.ply");
+    std::vector<array<double, 3>> vertices = plyIn.getVertexPositions();
+    std::vector<vector<size_t>> faces = plyIn.getFaceIndices<size_t>();
+    std::vector<Point3D> points;
+
+    for (const auto& i : vertices) 
+        {
+            Point3D p(i[0], i[1], i[2]);
+            points.push_back(p);
+        }
+    // for (auto p : points) 
+    //     {
+    //         std::cout << p << " ";
+    //     }
+    for (int i=0; i<points.size(); ++i)
+    std::cout <<points[i].to_string() << '\n';
+}
+
+
+
+
