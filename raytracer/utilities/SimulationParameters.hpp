d699c0241150c291f58dbc845c55e4595251022c
Owais Bin Asad <owaisbinasad.work@gmail.com>
2021-12-19 21:55:29 +0500

    missed merge file

--- raytracer/utilities/SimulationParameters.hpp
+++ raytracer/utilities/SimulationParameters.hpp
@@ -4,3 +4,5 @@ const int NPR = 1000; // Number of primary rays
 const bool blur = true;
 const float brightness_adjustment = 1.5;
 const bool to_accelerate = true;
+const bool lighting = true;
+const bool secondary_rays = true;
